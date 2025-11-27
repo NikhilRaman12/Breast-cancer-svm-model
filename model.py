f3baa6abee41ce8be4b8a59c5edb1fab9f5cb0cd
Nikhil Raman <nikhilraman1203@gmail.com>
2025-11-27 22:06:11 +0530

    Added patient_features example CSV

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 #import all the required libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
